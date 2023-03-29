25eeee83c673cba01a312c44979b1c3f8385f9f5
SergeySahar <seregasahar@yandex.ru>
2023-03-29 20:13:25 +0300

    KS-1-2 variable change and print

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,17 @@
+package lesson_1
+
+
+fun main() {
+
+    val quantityOfGoods = 75
+    val gratitudeMessage = "Have a good day"
+    println(quantityOfGoods)
+    println(gratitudeMessage)
+
+    var numberOfEmployees = 2000
+    // println(NumberOfEmployees)
+
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+
+}
