6212601926fb6fbcdf4a0bc07672aa3ef9be61d0
SergeySahar <seregasahar@yandex.ru>
2023-04-02 13:37:47 +0300

    KS-2-1 bookkeeping

---
+++ src/main/kotlin/lesson_2/lesson_2_task1.kt
@@ -0,0 +1,20 @@
+package lesson_2
+
+fun main() {
+
+    val a: Int = 50
+    val b: Int = 30000
+    val sum = (a * b)
+
+    val c: Int = 30
+    val d: Int = 20000
+    val sum1 = (c * d)
+
+    val e = (sum + sum1)
+    val f = (a + c)
+
+    println(sum)
+    println(sum + sum1)
+    println(e / f)
+
+}
