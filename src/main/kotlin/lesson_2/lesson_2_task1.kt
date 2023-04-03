cea01f39f01141df54952aa671ab93a7d8b5157f
SergeySahar <seregasahar@yandex.ru>
2023-04-03 20:13:06 +0300

    KS-2-1 cash

--- src/main/kotlin/lesson_2/lesson_2_task1.kt
+++ src/main/kotlin/lesson_2/lesson_2_task1.kt
@@ -2,19 +2,19 @@ package lesson_2
 
 fun main() {
 
-    val a: Int = 50
-    val b: Int = 30000
-    val sum = (a * b)
+    val workers: Int = 50
+    val cash: Int = 30000
+    val sum = (workers * cash) //сумма постоянная
 
-    val c: Int = 30
-    val d: Int = 20000
-    val sum1 = (c * d)
+    val intercessor: Int = 30
+    val lowCash: Int = 20000
+    val sum1 = (intercessor * lowCash) //сумма стажерская
 
     val e = (sum + sum1)
-    val f = (a + c)
+    val f = (workers + intercessor)
 
     println(sum)
     println(sum + sum1)
-    println(e / f)
+    println(e / f) //усредненная зп сотрудника
 
 }
