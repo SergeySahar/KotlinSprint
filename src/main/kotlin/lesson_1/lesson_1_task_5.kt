d94921766981f3a1f4a308245815b59adced22e1
SergeySahar <seregasahar@yandex.ru>
2023-04-03 08:55:18 +0300

    KS-1-5 bookkeeping

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -3,8 +3,9 @@ package lesson_1
 
 fun main() {
 
-    val seconds : Short = 6480
-    val minute : Byte = 108
+    val seconds: Short = 6480
+    val minSum = (seconds / 60)
+    val minute = (minSum)
 
     print(minute)
     print(":")
